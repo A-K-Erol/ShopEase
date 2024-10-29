cfce366de0a4ecc90e086bcab968347cf13d7ec0
Ansel Erol <43559753+A-K-Erol@users.noreply.github.com>
2024-10-29 14:11:54 -0400

    Add files via upload

---
+++ ShopEase-Java/src/main/java/com/example/ShopEase/Java/ShopEaseJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShopEase.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEaseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopEaseJavaApplication.class, args);
+	}
+
+}
---
+++ ShopEase-Java/src/test/java/com/example/ShopEase/Java/ShopEaseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopEase.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEaseJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
